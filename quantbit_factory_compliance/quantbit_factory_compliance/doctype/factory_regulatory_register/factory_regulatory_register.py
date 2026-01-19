384c1a7a07815e7a89275519d6ff0f7ad8c5338d
Sourabh Yelpale <sourabhyelpale29@gmail.com>
2026-01-19 13:26:41 +0530

    19 Jan 2026 - Doctype Validation and update

--- quantbit_factory_compliance/quantbit_factory_compliance/doctype/factory_regulatory_register/factory_regulatory_register.py
+++ quantbit_factory_compliance/quantbit_factory_compliance/doctype/factory_regulatory_register/factory_regulatory_register.py
@@ -7,7 +7,3 @@ from frappe.model.document import Document
 
 class FactoryRegulatoryRegister(Document):
     pass
-	# def before_save(self):
-	# 	due = self.due_day
-	# 	if(due < 1 or due  > 31):
-	# 		frappe.log_error("Enter date between 1-31")
