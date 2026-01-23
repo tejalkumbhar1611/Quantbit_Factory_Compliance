966bbe824cf0afa952f845d69d636c1fdbafec1b
Sourabh Yelpale <sourabhyelpale29@gmail.com>
2026-01-23 10:29:12 +0530

    23 Jan 2026 - Changes in Fixtures

--- quantbit_factory_compliance/hooks.py
+++ quantbit_factory_compliance/hooks.py
@@ -11,43 +11,10 @@ fixtures = [
         "dt": "Number Card",
         "filters": [
             ["name", "in", [
-                "High Risk Cases"
-            ]]
-        ]
-    },
-    {
-        "dt": "Dashboard Chart",
-        "filters": [
-            ["name", "in", [
-                "Liability Exposure (Sum)",
-                "Open vs Closed Cases",
-                "Upcoming Hearings"
-            ]]
-        ]
-    },
-    {
-        "dt": "Number Card",
-        "filters": [
-            ["name", "in", [
+                "High Risk Cases",
                 "Active Licenses",
                 "Expired Licenses",
-                "Licenses Under Renewal"
-            ]]
-        ]
-    },
-    {
-        "dt": "Dashboard Chart",
-        "filters": [
-            ["name", "in", [
-                "Licenses Expiry Next 30 60 90 Days",
-                "License Renewals Per Month"
-            ]]
-        ]
-    },
-    {
-        "dt": "Number Card",
-        "filters": [
-            ["name", "in", [
+                "Licenses Under Renewal",
                 "Completed This Month",
                 "Overdue Compliances",
                 "Due in Next 30 Days",
@@ -59,6 +26,11 @@ fixtures = [
         "dt": "Dashboard Chart",
         "filters": [
             ["name", "in", [
+                "Liability Exposure (Sum)",
+                "Open vs Closed Cases",
+                "Upcoming Hearings",
+                "Licenses Expiry Next 30 60 90 Days",
+                "License Renewals Per Month",
                 "Compliance Status",
                 "Compliance by Authority",
                 "Risk Level",
@@ -69,148 +41,20 @@ fixtures = [
 ]
 
 
-# Apps
-# ------------------
-# required_apps = []
 
-# Each item in the list will be shown as an app in the apps page
-# add_to_apps_screen = [
-# 	{
-# 		"name": "quantbit_factory_compliance",
-# 		"logo": "/assets/quantbit_factory_compliance/logo.png",
-# 		"title": "Quantbit Factory Compliance",
-# 		"route": "/quantbit_factory_compliance",
-# 		"has_permission": "quantbit_factory_compliance.api.permission.has_app_permission"
-# 	}
-# ]
-
-# Includes in <head>
-# ------------------
-
-# include js, css files in header of desk.html
-# app_include_css = "/assets/quantbit_factory_compliance/css/quantbit_factory_compliance.css"
-# app_include_js = "/assets/quantbit_factory_compliance/js/quantbit_factory_compliance.js"
-
-# include js, css files in header of web template
-# web_include_css = "/assets/quantbit_factory_compliance/css/quantbit_factory_compliance.css"
-# web_include_js = "/assets/quantbit_factory_compliance/js/quantbit_factory_compliance.js"
-
-# include custom scss in every website theme (without file extension ".scss")
-# website_theme_scss = "quantbit_factory_compliance/public/scss/website"
-
-# include js, css files in header of web form
-# webform_include_js = {"doctype": "public/js/doctype.js"}
-# webform_include_css = {"doctype": "public/css/doctype.css"}
-
-# include js in page
-# page_js = {"page" : "public/js/file.js"}
-
-# include js in doctype views
-# doctype_js = {"doctype" : "public/js/doctype.js"}
-# doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
-# doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
-# doctype_calendar_js = {"doctype" : "public/js/doctype_calendar.js"}
-
-# Svg Icons
-# ------------------
-# include app icons in desk
-# app_include_icons = "quantbit_factory_compliance/public/icons.svg"
-
-# Home Pages
-# ----------
-
-# application home page (will override Website Settings)
-# home_page = "login"
-
-# website user home page (by Role)
-# role_home_page = {
-# 	"Role": "home_page"
-# }
-
-# Generators
-# ----------
-
-# automatically create page for each record of this doctype
-# website_generators = ["Web Page"]
-
-# Jinja
-# ----------
-
-# add methods and filters to jinja environment
-# jinja = {
-# 	"methods": "quantbit_factory_compliance.utils.jinja_methods",
-# 	"filters": "quantbit_factory_compliance.utils.jinja_filters"
-# }
-
-# Installation
-# ------------
-
-# before_install = "quantbit_factory_compliance.install.before_install"
-# after_install = "quantbit_factory_compliance.install.after_install"
-
-# Uninstallation
-# ------------
-
-# before_uninstall = "quantbit_factory_compliance.uninstall.before_uninstall"
-# after_uninstall = "quantbit_factory_compliance.uninstall.after_uninstall"
-
-# Integration Setup
-# ------------------
-# To set up dependencies/integrations with other apps
-# Name of the app being installed is passed as an argument
-
-# before_app_install = "quantbit_factory_compliance.utils.before_app_install"
-# after_app_install = "quantbit_factory_compliance.utils.after_app_install"
-
-# Integration Cleanup
-# -------------------
-# To clean up dependencies/integrations with other apps
-# Name of the app being uninstalled is passed as an argument
-
-# before_app_uninstall = "quantbit_factory_compliance.utils.before_app_uninstall"
-# after_app_uninstall = "quantbit_factory_compliance.utils.after_app_uninstall"
-
-# Desk Notifications
-# ------------------
-# See frappe.core.notifications.get_notification_config
-
-# notification_config = "quantbit_factory_compliance.notifications.get_notification_config"
-
-# Permissions
-# -----------
-# Permissions evaluated in scripted ways
-
-# permission_query_conditions = {
-# 	"Event": "frappe.desk.doctype.event.event.get_permission_query_conditions",
-# }
-#
-# has_permission = {
-# 	"Event": "frappe.desk.doctype.event.event.has_permission",
-# }
-
-# DocType Class
-# ---------------
-# Override standard doctype classes
-
-# override_doctype_class = {
-# 	"ToDo": "custom_app.overrides.CustomToDo"
-# }
-
-# Document Events
-# ---------------
-# Hook on document methods and events
-
-# doc_events = {
-# 	"*": {
-# 		"on_update": "method",
-# 		"on_cancel": "method",
-# 		"on_trash": "method"
-# 	}
-# }
+doc_events = {
+    "Compliance Master": {
+        "on_update": "quantbit_factory_compliance.quantbit_factory_compliance.doctype.compliance_master.compliance_master.on_update"
+    },
+    "License Master": {
+        "on_update": "quantbit_factory_compliance.quantbit_factory_compliance.doctype.license.license.on_update"
+    }
+}
 
 
 scheduler_events = {
     "daily": [
+        "quantbit_factory_compliance.quantbit_factory_compliance.schedulers.expire_license_frr",
         "quantbit_factory_compliance.quantbit_factory_compliance.schedulers.create_compliance_task",
         "quantbit_factory_compliance.quantbit_factory_compliance.schedulers.create_license_task",
         "quantbit_factory_compliance.quantbit_factory_compliance.schedulers.overdue_status_compliance_task",
@@ -235,112 +79,4 @@ scheduler_events = {
 #     }
 # }
 
-
-doc_events = {
-    "Compliance Master": {
-        "on_update": "quantbit_factory_compliance.quantbit_factory_compliance.doctype.compliance_master.compliance_master.on_update"
-    },
-    "License Master": {
-        "on_update": "quantbit_factory_compliance.quantbit_factory_compliance.doctype.license.license.on_update"
-    }
-}
-
-# scheduler_events = {
-# 	"all": [
-# 		"quantbit_factory_compliance.tasks.all"
-# 	],
-# 	"daily": [
-# 		"quantbit_factory_compliance.tasks.daily"
-# 	],
-# 	"hourly": [
-# 		"quantbit_factory_compliance.tasks.hourly"
-# 	],
-# 	"weekly": [
-# 		"quantbit_factory_compliance.tasks.weekly"
-# 	],
-# 	"monthly": [
-# 		"quantbit_factory_compliance.tasks.monthly"
-# 	],
-# }
-
-# Testing
-# -------
-
-# before_tests = "quantbit_factory_compliance.install.before_tests"
-
-# Overriding Methods
-# ------------------------------
-#
-# override_whitelisted_methods = {
-# 	"frappe.desk.doctype.event.event.get_events": "quantbit_factory_compliance.event.get_events"
-# }
-#
-# each overriding function accepts a `data` argument;
-# generated from the base implementation of the doctype dashboard,
-# along with any modifications made in other Frappe apps
-# override_doctype_dashboards = {
-# 	"Task": "quantbit_factory_compliance.task.get_dashboard_data"
-# }
-
-# exempt linked doctypes from being automatically cancelled
-#
-# auto_cancel_exempted_doctypes = ["Auto Repeat"]
-
-# Ignore links to specified DocTypes when deleting documents
-# -----------------------------------------------------------
-
-# ignore_links_on_delete = ["Communication", "ToDo"]
-
-# Request Events
-# ----------------
-# before_request = ["quantbit_factory_compliance.utils.before_request"]
-# after_request = ["quantbit_factory_compliance.utils.after_request"]
-
-# Job Events
-# ----------
-# before_job = ["quantbit_factory_compliance.utils.before_job"]
-# after_job = ["quantbit_factory_compliance.utils.after_job"]
-
-# User Data Protection
-# --------------------
-
-# user_data_fields = [
-# 	{
-# 		"doctype": "{doctype_1}",
-# 		"filter_by": "{filter_by}",
-# 		"redact_fields": ["{field_1}", "{field_2}"],
-# 		"partial": 1,
-# 	},
-# 	{
-# 		"doctype": "{doctype_2}",
-# 		"filter_by": "{filter_by}",
-# 		"partial": 1,
-# 	},
-# 	{
-# 		"doctype": "{doctype_3}",
-# 		"strict": False,
-# 	},
-# 	{
-# 		"doctype": "{doctype_4}"
-# 	}
-# ]
-
-# Authentication and authorization
-# --------------------------------
-
-# auth_hooks = [
-# 	"quantbit_factory_compliance.auth.validate"
-# ]
-
-# Automatically update python controller files with type annotations for this app.
-# export_python_type_annotations = True
-
-# default_log_clearing_doctypes = {
-# 	"Logging DocType Name": 30  # days to retain logs
-# }
-
-# Translation
-# ------------
-# List of apps whose translatable strings should be excluded from this app's translations.
-# ignore_translatable_strings_from = []
 
